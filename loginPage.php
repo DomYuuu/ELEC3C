658b07386768fc5c44c2488e35a7c8b30fc4a802
DomYuuu <Dominicyu45@gmail.com>
2022-12-06 22:34:28 +0800

    cr8 done

--- loginPage.php
+++ loginPage.php
@@ -11,15 +11,15 @@ if(isset($_POST['submit']))
 
 <!DOCTYPE html>
 <html>
-<link rel="stylesheet" href="style.css">
     <head>
-    <meta charset="ISO-8859-1">
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <meta charset="ISO-8859-1">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Lab Act</title>
     </head>
-    <body class="">
+    <body class="signPage">
     <div class="container">
 
         <div class="row justify-content-center">
@@ -44,7 +44,7 @@ if(isset($_POST['submit']))
 
                     <p>USERNAME<input class="form-control" type="text" name="uname" placeholder="Enter your Username..." required></p>
                     <p>PASSWORD<input class="form-control" type="password" name="pass" placeholder="Enter your Password..." required></p>
-                    <br>
+                    <p style="text-align: center"; class="text-decoration-none"><a href="createAcc.html">Don't have an Account?</a></p>
                     <input class="form-control btn btn-outline-warning" type="submit" name="submit" value="Login">
                  </form>
              </div>
